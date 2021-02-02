d451b4984f40c75442bcab9ffb8a44d6ec98c230
Abdul Razaq <razaqabdul8@gmail.com>
2021-02-02 16:21:30 -0500

    removed loading attribute add percent loaded to progress bar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const progressBar = document.getElementsByClassName
 ('progress-bar')[0]
-setInterval(() => {
+/* create variable for interval */
+var loadingProgress = setInterval(() => {
     const computedStyle = getComputedStyle(progressBar)
-    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
-    progressBar.style.setProperty('--width', width + .1)
-}, 5)
+    const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+    // Get max attribute
+    const max = parseFloat(progressBar.getAttribute('max')) || 0;
+    if(width > 100 || (max > 0 && max < width)){ // Check if loaded 100% or incrementing to max attribute
+        clearInterval(loadingProgress);
+    }
+    else{
+        progressBar.style.setProperty('--width', width + .1);
+        // Add percent loaded to progress bar
+        progressBar.setAttribute('data-label',width+'%');
+    }
+}, 20)
